bd1b9e9318f4758894af4fe4dd22fcd0fb157a30
Maurits Schelvis <s1156856@student.hsleiden.nl>
2026-05-06 18:08:49 +0200

    small changes

--- apps/backend/src/main/java/com/mauri/backend/controller/PatientController.java
+++ apps/backend/src/main/java/com/mauri/backend/controller/PatientController.java
@@ -12,13 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/patients")
+@RequestMapping(value = "/api/patients", produces = MediaType.APPLICATION_JSON_VALUE)
 public class PatientController {
 
     private final PatientService patientService;
